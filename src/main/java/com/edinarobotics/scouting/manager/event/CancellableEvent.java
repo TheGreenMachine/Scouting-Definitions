9537918ab31bed6c6361f368b819c720b3fecbae
Karl Otness <karl@otness.com>
2012-06-14 20:44:39 -0500

    Add CancellableEvent to the project.
    This class makes it easier to implement Event subclasses
    that implement the Cancellable interface.

---
+++ src/main/java/com/edinarobotics/scouting/manager/event/CancellableEvent.java
@@ -0,0 +1,18 @@
+package com.edinarobotics.scouting.manager.event;
+
+/**
+ * This class represents events that can be cancelled throughout the scouting system.
+ * It makes implementing classes extending {@link Event} and implementing
+ * {@link Cancellable} simpler.
+ */
+public abstract class CancellableEvent extends Event implements Cancellable {
+	private boolean cancelled = false;
+	
+	public boolean isCancelled(){
+		return cancelled;
+	}
+	
+	public void setCancelled(boolean cancel){
+		cancelled = cancel;
+	}
+}
