2a4be343d6876a4722d15662500177649e1e7acc
Karl Otness <karl@otness.com>
2012-06-15 15:04:50 -0500

    Add Data interface to be used as a parent marker for all data types.
    This interface will be used only as a marker interface for any
    data types available in the scouting system.

---
+++ src/main/java/com/edinarobotics/scouting/manager/database/types/Data.java
@@ -0,0 +1,11 @@
+package com.edinarobotics.scouting.manager.database.types;
+
+/**
+ * Interface used as the parent for all data types available for
+ * use in scouting data.<br/>
+ * <em>This interface is intended only as a marker
+ * for included data types. Do not implement it!</em>
+ */
+public interface Data {
+	
+}
