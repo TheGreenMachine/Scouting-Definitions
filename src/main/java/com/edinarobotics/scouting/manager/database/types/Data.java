e14751da95f9976248b2be032e493c8fea45eb96
Karl Otness <karl@otness.com>
2012-06-17 18:29:03 -0500

    Add serializeToString() method to the Data interface.
    This method will be used to provide a safe way
    to turn all Data objects into String objects.

--- src/main/java/com/edinarobotics/scouting/manager/database/types/Data.java
+++ src/main/java/com/edinarobotics/scouting/manager/database/types/Data.java
@@ -3,9 +3,17 @@ package com.edinarobotics.scouting.manager.database.types;
 /**
  * Interface used as the parent for all data types available for
  * use in scouting data.<br/>
- * <em>This interface is intended only as a marker
+ * <em>This interface is intended mostly as a marker
  * for included data types. Do not implement it!</em>
  */
 public interface Data {
 	
+	/**
+	 * Provides a {@link String} representation of a
+	 * {@link Data} object's state that can be used to
+	 * reconstruct it.
+	 * @return A {@link String} representing this object's
+	 * current state.
+	 */
+	public String serializeToString();
 }
