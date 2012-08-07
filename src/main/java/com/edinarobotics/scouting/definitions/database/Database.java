1e1e8018cf7403f9cee32a3d1e0d60614a0fc419
Karl Otness <karl@otness.com>
2012-08-06 22:49:09 -0500

    Add Database to the project.
    This interface defines the contract of database implementations.
    It provides the methods for launching queries and database change
    operations.

---
+++ src/main/java/com/edinarobotics/scouting/definitions/database/Database.java
@@ -0,0 +1,86 @@
+package com.edinarobotics.scouting.definitions.database;
+
+import com.edinarobotics.scouting.definitions.database.changes.Transaction;
+import com.edinarobotics.scouting.definitions.database.queries.Query;
+import com.edinarobotics.scouting.definitions.event.Future;
+
+/**
+ * This class provides access to various
+ * database implementations. It provides the necessary functions
+ * to query and modify database values.
+ * <br/>
+ * Implementing a {@link Database} should be done carefully. All
+ * {@link Database} implementations must behave in the same way and must
+ * follow all method contracts.
+ * <br/>
+ * Database objects are expected to adhere to the
+ * <a href=http://en.wikipedia.org/wiki/ACID>ACID</a> principles.
+ * <h3>Database Metadata</h3>
+ * To retrieve a list of all currently defined tables, query
+ * the {@code @tables} metatable. Its results will have
+ * an {@link com.edinarobotics.scouting.definitions.database.types.Integer Integer}
+ * primary key value (the database table id number) and a 
+ * {@link com.edinarobotics.scouting.definitions.database.types.Text Text}
+ * table name in the {@code tid} and {@code tname} columns respectively.
+ * <br/><br/>
+ * To retrieve schema information for a table, query the {@code @schema}
+ * metatable. The results will have an
+ * {@link com.edinarobotics.scouting.definitions.database.types.Integer Integer}
+ * column primary key id value, an
+ * {@link com.edinarobotics.scouting.definitions.database.types.IntegerForeignKey IntegerForeignKey}
+ * value pointing to the table id from the {@code @tables} metatable and
+ * a {@link com.edinarobotics.scouting.definitions.database.types.Text Text}
+ * value defining the type of the column.<br/>
+ * Possible type values are:
+ * <ul>
+ * <li>{@code blob}</li>
+ * <li>{@code boolean}</li>
+ * <li>{@code integer}</li>
+ * <li>{@code integer foreign key <table reference string>}</li>
+ * <li>{@code integer primary key}</li>
+ * <li>{@code real}</li>
+ * <li>{@code text}</li>
+ * </ul>
+ * These values will be in the {@code cid}, {@code tid} and {@code ctype}
+ * columns respectively. The results will include the schemas of the
+ * {@code @tables} and {@code @schema} metatables.
+ * The same information can also
+ * be retrieved by sampling a {@link Row} from a table and consulting its
+ * {@link Column} objects' types.
+ */
+public interface Database {
+	
+	/**
+	 * This method is used to launch a {@link Query} on this Database.
+	 * <br/>
+	 * Queries are executed in the order they are received by the database
+	 * (see the information on the ACID principles in {@link Database}).
+	 * The database access API is thread-safe and uses {@link Future} objects
+	 * to return the results of database operations. The {@link RowSet} values
+	 * can be retrieved by the Future object's {@link Future#get() get()} method.
+	 * However, this method will throw different exceptions depending on the
+	 * results of the database transaction.
+	 * @param query The Query object representing the desired parameters
+	 * for the {@link Row} objects to return.
+	 * @return A {@link Future} object containing the result of the query
+	 * (if the query succeeds).
+	 */
+	public abstract Future<RowSet> executeQuery(Query query);
+	
+	/**
+	 * This method is used to execute a {@link Transaction} on the Database.
+	 * <br/>
+	 * Transactions are executed in the order they are received by the database
+	 * (see the information on the ACID principles in {@link Database}).
+	 * The {@link Future} object returned by this method will always return
+	 * {@code null} from its {@link Future#get() get()} method. However,
+	 * exceptions may still be thrown to indicate a failure in the execution the given
+	 * transaction. Use the {@link Future#get() Future.get()} method to join the
+	 * transaction after its completion or failure.
+	 * @param transaction The {@link Transaction} to be applied to the Database.
+	 * @return A {@link Future} object that can be used to inspect the results
+	 * of the transaction. The Future object always contains a {@code null}
+	 * result value.
+	 */
+	public abstract Future<?> executeTransaction(Transaction transaction);
+}
