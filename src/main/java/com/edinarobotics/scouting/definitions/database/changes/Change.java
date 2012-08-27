f7b7d8d8372553b97a3c94f08e7d3ff45e052fba
Karl Otness <karl@otness.com>
2012-08-26 22:44:14 -0500

    Add getTargetTableReference() and getTargetTableName() to Change.
    These methods can be used to easily determine what table is targeted
    by a given Change object.

--- src/main/java/com/edinarobotics/scouting/definitions/database/changes/Change.java
+++ src/main/java/com/edinarobotics/scouting/definitions/database/changes/Change.java
@@ -1,5 +1,7 @@
 package com.edinarobotics.scouting.definitions.database.changes;
 
+import com.edinarobotics.scouting.definitions.database.references.TableReference;
+
 /**
  * This is a marker interface for all database
  * Change operations. Classes implementing this
@@ -12,5 +14,18 @@ package com.edinarobotics.scouting.definitions.database.changes;
  * can cause severe problems. <em>Do not</em> implement this interface.
  */
 public interface Change {
-
+	/**
+	 * Returns the {@link TableReference} of the table targeted by this
+	 * Change implementation. The definition of "targeted" depends on the
+	 * Change implementation.
+	 * @return The TableReference of the table targeted by this Change.
+	 */
+	public TableReference getTargetTableReference();
+	
+	/**
+	 * Returns the name of the table targeted by this Change implementation.
+	 * The definition of "targeted" depends on the Change implementation.
+	 * @return The {@code String} name of the table targeted by this Change.
+	 */
+	public String getTargetTableName();
 }
