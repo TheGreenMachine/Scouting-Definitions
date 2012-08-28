5613d840ebcfa67483e3c206f5d67235806eac6c
Karl Otness <karl@otness.com>
2012-08-28 00:36:48 -0500

    Add Listener to the project.
    Listener is a marker interface used to indicate classes that contain
    annotated EventListener methods. Listener implementations
    can be used in the event system.

---
+++ src/main/java/com/edinarobotics/scouting/definitions/event/Listener.java
@@ -0,0 +1,11 @@
+package com.edinarobotics.scouting.definitions.event;
+
+/**
+ * This is an empty marker interface for classes with methods
+ * annotated as {@link EventListener} implementations.
+ * This interface allows the implementing class to register its
+ * annotated event listeners with an {@link EventRegistrar}.
+ */
+public interface Listener {
+
+}
