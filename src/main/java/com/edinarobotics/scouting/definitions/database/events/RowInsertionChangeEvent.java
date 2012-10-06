a89013236bf6008ca72fd3e653a1a673d9aa641f
Karl Otness <karl@otness.com>
2012-10-06 14:24:27 -0500

    Add RowInsertionChangeEvent to the project.
    This event will be fired whenever a RowInsertionChange is submitted.

---
+++ src/main/java/com/edinarobotics/scouting/definitions/database/events/RowInsertionChangeEvent.java
@@ -0,0 +1,40 @@
+package com.edinarobotics.scouting.definitions.database.events;
+
+import com.edinarobotics.scouting.definitions.database.changes.RowInsertionChange;
+
+/**
+ * This event is fired by {@link com.edinarobotics.scouting.definitions.database.Database Database}
+ * implementations whenever a table is modified by a {@link RowInsertionChange} object in a
+ * {@link com.edinarobotics.scouting.definitions.database.changes.Transaction Transaction}.
+ */
+public class RowInsertionChangeEvent extends DatabaseChangeEvent{
+	
+	/**
+	 * Constructs a RowInsertionChangeEvent representing the given
+	 * {@link RowInsertionChange} object.
+	 * @param change The RowInsertionChange to be stored in this
+	 * RowInsertionChangeEvent.
+	 */
+	public RowInsertionChangeEvent(RowInsertionChange change){
+		super(change);
+	}
+	
+	/**
+	 * Constructs a RowInsertionChangeEvent representing the given {@link RowInsertionChange} object and
+	 * having the given String ID value.
+	 * @param change The RowInsertionChange to be stored in this RowInsertionChangeEvent.
+	 * @param id The String ID value to be assigned to this event.
+	 * @see CancellableEvent#CancellableEvent(String)
+	 */
+	public RowInsertionChangeEvent(RowInsertionChange change, String id){
+		super(change, id);
+	}
+	
+	/**
+	 * Allows access to the {@link RowInsertionChange} object stored in this RowInsertionChangeEvent.
+	 * @return The RowInsertionChange object stored in this RowInsertionChangeEvent.
+	 */
+	public RowInsertionChange getChange(){
+		return (RowInsertionChange)super.getChange();
+	}
+}
