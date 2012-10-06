b8707587549954c4a716339fefd6209d8c519d0f
Karl Otness <karl@otness.com>
2012-10-06 14:18:35 -0500

    Add documentation to CancellableEvent and fix constructor issues.
    The new documentation documents the constructors. The new constuctor
    is the CancellableEvent(String id) constructor.

--- src/main/java/com/edinarobotics/scouting/definitions/event/CancellableEvent.java
+++ src/main/java/com/edinarobotics/scouting/definitions/event/CancellableEvent.java
@@ -8,6 +8,24 @@ package com.edinarobotics.scouting.definitions.event;
 public abstract class CancellableEvent extends Event implements Cancellable {
 	private boolean cancelled = false;
 	
+	/**
+	 * Constructs a new CancellableEvent. This constructor assigns
+	 * a random, unique ID value to this event.
+	 * @see Event#Event()
+	 */
+	public CancellableEvent(){
+		super();
+	}
+	
+	/**
+	 * Constructs a new CancellableEvent with a specified ID value.
+	 * This constructor assigns the specified ID value to this event.
+	 * @param id The String ID value to be assigned to this event.
+	 */
+	public CancellableEvent(String id){
+		super(id);
+	}
+	
 	public boolean isCancelled(){
 		return cancelled;
 	}
