ec6c32da908c4628af7b13d0bbb223c93a7fbc3d
Karl Otness <karl@otness.com>
2013-01-10 18:22:32 -0600

    Add EventMonitor to the project.
    The EventMonitor annotation is used to indicate methods that are
    to be notified of the outcome of events. These methods receive
    the Event object that was fired as well as a Result value
    representing the outcome of the action.

---
+++ src/main/java/com/edinarobotics/scouting/definitions/event/EventMonitor.java
@@ -0,0 +1,23 @@
+package com.edinarobotics.scouting.definitions.event;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.Retention;
+
+/**
+ * This annotation is used to mark class methods
+ * that act as {@link Event} monitors.
+ * The type of event to which this monitor is bound is determined
+ * by reflection. It is the responsibility of the event firing
+ * class to properly bind these monitors.<br/>
+ * Event monitors receive both the fired Event object and
+ * the {@link Result} of the action but have no say in whether
+ * or not the event is cancelled. They are notified <em>after</em>
+ * the action has finished.
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EventMonitor {
+
+}
